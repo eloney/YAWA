33f7f9d422d5c7ffbdafc9fc50477a4e9beb56ec
Ben Zhong <ben0168@hotmail.com>
2015-03-12 14:38:55 +1300

    home page layout main activity

--- app/src/main/java/interview/mea/yawa/YAWA.java
+++ app/src/main/java/interview/mea/yawa/YAWA.java
@@ -1,17 +1,62 @@
 package interview.mea.yawa;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class YAWA extends Activity {
+public class YAWA extends Activity implements View.OnClickListener {
+
+    private List<String> dayList = new ArrayList<String>();
+    private int dayCount = 16;
+    private int defaultDays = 7;
+    private EditText cityNameText;
+    private Spinner daySpinner;
+    private ArrayAdapter<String> dayAdapter;
+    private Button checkWeatherButton;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yawa);
+
+
+        //initialize dayList
+        for (int i = 1;i <= dayCount; i++){
+            if (i == 1){
+                dayList.add(i + "day");
+            } else {
+                dayList.add(i + "days");
+            }
+        }
+
+        cityNameText = (EditText)findViewById(R.id.EditText_cityName);
+        daySpinner = (Spinner)findViewById(R.id.Spinner_Days);
+        checkWeatherButton = (Button)findViewById(R.id.Button_CheckWeather);
+
+        dayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,dayList);
+        daySpinner.setAdapter(dayAdapter);
+        daySpinner.setSelection(defaultDays-1);
+
+
+
+        findViewById(R.id.Activity_YAWA_main).setOnClickListener(this);
+
     }
 
 
@@ -35,5 +80,18 @@ public class YAWA extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //hide keyboard when click screen
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.Activity_YAWA_main:
+                InputMethodManager imm = (InputMethodManager)
+                        getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                break;
+        }
+
     }
 }
